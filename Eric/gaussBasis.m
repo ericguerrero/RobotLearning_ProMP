63e1609d30a151a1a51b7e31e727d80be24052c5
NicolaCov <nicolacovallero92@gmail.com>
2015-11-05 18:01:34 +0100

    REPO REORGANIZATION Main source files inside src/. The others utilities functions are in util/.

--- Eric/gaussBasis.m
+++
@@ -1,8 +0,0 @@
-function val = gaussian1D(x, mu, sigma)
-%  x     - Input vector
-%  mu    - Mean
-%  sigma - Standard deviation
-
-    % Evaluate a 1D gaussian.
-    val = exp(-(x - mu).^2 ./ (2 * sigma^2));
-end
--- Nicola/README.md
+++
@@ -1,1 +0,0 @@
-# RobotLearning_ProMP
