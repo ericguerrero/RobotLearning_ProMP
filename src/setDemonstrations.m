1cf58654174a0c3e0c88a5eaf262ceddbb097925
ericguerrero <ericguerrerofont@gmail.com>
2015-11-10 11:40:41 +0100

    setDemonstration structuraly modified inputs outputs between main.m and setDemonstrations modified, also added main2D.m to dieal with 2 dimensions

--- src/setDemonstrations.m
+++ src/setDemonstrations.m
@@ -1,14 +1,14 @@
-function [time, trajDemo] = setDemonstrations(num,points,samples)
-time = linspace(0,1,samples)';
-n = size(points,2);
-trajDemo = zeros(samples,num);
+function [coordDemo] = setDemonstrations(num,coord,time,samples)
+
+n = size(coord,2);
+coordDemo = zeros(samples,num);
 for i=1:num
     % Add noise
     r = randn(1,n); 
-    x =  points(2,:) +  r/10;
+    x =  coord(2,:) +  r/10;
     
     %Create Demonstration
-    trajDemo(:,i) = spline(points(1,:),x,time); % spline
+   coordDemo(:,i) = spline(coord(1,:),x,time); % spline
 end
 end
 
